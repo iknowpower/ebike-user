8bacd9e7327e1ba2c0f0299f56a17a55920c7660
sfy666666 <1352324022@qq.com>
2020-09-27 18:14:40 +0800

    sfy commit

--- pages/message/message.js
+++ pages/message/message.js
@@ -5,48 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-    count: 1,
+    count: 0,
     info: [
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      },
-      {
-        'chargeFinishTime': '2020-08-08 17:56:34',
-        'chargeAddr': '钱农西路2#充电站 4号插座',
-        'finishReasonDesc': '插头未连接',
-      }
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // },
+      // {
+      //   'chargeFinishTime': '2020-08-08 17:56:34',
+      //   'chargeAddr': '钱农西路2#充电站 4号插座',
+      //   'finishReasonDesc': '插头未连接',
+      // }
      
     ],
   },
@@ -56,18 +56,18 @@ Page({
    */
   onLoad: function () {
     var that = this;
-    // wx.request({
-    //   url: app.httpUrl + '/ebike-charge/logMsg/getLogList.x',
-    //   data: {
-    //     sessionid: app.globalData.sessionid
-    //   },
-    //   success: (re) => {
-    //       that.setData({
-    //         info:re.data.info,
-    //         count:re.data.count
-    //       })
-    //   }
-    // });
+    wx.request({
+      url: app.httpUrl + '/ebike-charge/logMsg/getLogList.x',
+      data: {
+        sessionid: app.globalData.sessionid
+      },
+      success: (re) => {
+          that.setData({
+            info:re.data.info,
+            count:re.data.count
+          })
+      }
+    });
   },
 
   goHis() {
